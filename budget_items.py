af4193b01242bcd77d2013e0e2adcbf7d15dbab5
Andrew Ghorbani <andrewbghorbani@gmail.com>
2023-05-01 14:44:46 -0700

    add classes for budget categories and continue work on categorizing spending

--- budget_items.py
+++ budget_items.py
@@ -1,4 +1,17 @@
-class Needs:
+class Category:
+
+    @property
+    def total_value(self):
+        total_value = 0
+        attributes = vars(self)
+
+        for key, value in attributes.items():
+            total_value += value
+
+        return total_value
+
+
+class Needs(Category):
     def __init__(self):
         self.mortgage = 0
         self.rent = 0
@@ -15,11 +28,45 @@ class Needs:
         self.dental_insurance = 0
         self.vision_insurance = 0
         self.student_loans = 0
+        self.emergencies = 0
 
-class Wants:
-    def __init__(self):
-        pass
 
-class Savings:
+class Wants(Category):
     def __init__(self):
-        pass
+        self.free_spending = 0
+        self.subscriptions = 0
+        self.vacation_spending = 0
+
+
+class Savings(Category):
+    def __init__(self):
+        self.crypto = 0
+        self.emergency_fund = 0
+        self.retirement = 0
+        self.investing = 0
+
+
+class BudgetSpending:
+    def __init__(self):
+        self.needs = Needs()
+        self.wants = Wants()
+        self.savings = Savings()
+
+
+if __name__ == "__main__":
+
+    monthly_spending = BudgetSpending()
+
+    monthly_spending.needs.electric_bill = 10
+    monthly_spending.needs.health_insurance = 72.15
+    monthly_spending.needs.rent = 1500.72
+
+    monthly_spending.wants.free_spending = 499.99
+    monthly_spending.wants.subscriptions = 222.11
+
+    monthly_spending.savings.retirement = 250.00
+    monthly_spending.savings.emergency_fund = 100
+
+    print(monthly_spending.needs.total_value)
+    print(monthly_spending.wants.total_value)
+    print(monthly_spending.savings.total_value)
--- compile_data.py
+++ compile_data.py
@@ -1,15 +1,59 @@
 import pandas as pd
-import os
 from clean_tsv_data import clean_data, CLEANED_TSV_FILEPATH
+from budget_items import BudgetSpending
 
 
-def main():
+def load_data_to_dataframe(verbose=False):
+    """
+    Cleans the data from spending.tsv and loads into a DataFrame object.
+
+    :param verbose: True to print out the DataFrame; False otherwise.
+    :return: DataFrame object containing the data from cleaned_spending.tsv.
+    """
 
     if clean_data():
         raise RuntimeError("Something went wrong when trying to clean the data!")
 
     df = pd.read_table(CLEANED_TSV_FILEPATH, sep="\t")
-    print(df)
+
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+    df['Gains/Expenses'] = pd.to_numeric(df['Gains/Expenses'])
+    df['Checking Balance'] = pd.to_numeric(df['Checking Balance'])
+
+    if verbose:
+        print(df)
+
+    return df
+
+
+def categorize_spending(spending_df, date_range=["1/1/2021, 1/1/2022"]):
+    """
+
+    :param spending_df: DataFrame object containing the data from cleaned_spending.tsv
+    :param date_range: [minimum date to include, maximum date to exclude]
+    :return: BudgetSpending object
+    """
+
+    budget = BudgetSpending()
+    filtered_spending_df = spending_df[(spending_df["Date"] >= date_range[0]) & (spending_df["Date"] < date_range[1])]
+
+    for index, transaction in filtered_spending_df.iterrows():
+
+        if transaction["Category"] == "Needs":
+            pass
+
+        elif transaction["Category"] == "Wants":
+            pass
+
+        elif transaction["Category"] == "Savings":
+            pass
+
+
+def main():
+
+    spending_df = load_data_to_dataframe(verbose=False)
+    date_range = ["4/1/2023", "5/1/2023"]
+    categorize_spending(spending_df, date_range)
 
 if __name__ == "__main__":
     main()
