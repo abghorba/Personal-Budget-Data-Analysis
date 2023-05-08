9b0d89336709d16f3eee89a19aa04980d83ab72c
Andrew Ghorbani <andrewbghorbani@gmail.com>
2023-05-08 01:35:04 -0700

    update get_spending_data.py to change the data to view more easily

--- get_spending_data.py
+++ get_spending_data.py
@@ -8,39 +8,44 @@ def main():
     # At this point, we can do whatever data analysis we'd like
     analyzer = SpendingAnalyzer(budget_dict)
 
+    category = "needs"
+    subcategory = "gasoline"
+
+    print(f"\nGetting data for {category.upper()} > {subcategory.upper()}")
+
     print("\nAverages:")
-    print(analyzer.analysis["2021"]["needs"]["rent"]["average"])
-    print(analyzer.analysis["2022"]["needs"]["rent"]["average"])
-    print(analyzer.analysis["2023"]["needs"]["rent"]["average"])
-    print(analyzer.analysis["lifetime"]["needs"]["rent"]["average"])
+    print(analyzer.analysis["2021"][category][subcategory]["average"])
+    print(analyzer.analysis["2022"][category][subcategory]["average"])
+    print(analyzer.analysis["2023"][category][subcategory]["average"])
+    print(analyzer.analysis["lifetime"][category][subcategory]["average"])
 
     print("\nMedians:")
-    print(analyzer.analysis["2021"]["needs"]["rent"]["median"])
-    print(analyzer.analysis["2022"]["needs"]["rent"]["median"])
-    print(analyzer.analysis["2023"]["needs"]["rent"]["median"])
-    print(analyzer.analysis["lifetime"]["needs"]["rent"]["median"])
+    print(analyzer.analysis["2021"][category][subcategory]["median"])
+    print(analyzer.analysis["2022"][category][subcategory]["median"])
+    print(analyzer.analysis["2023"][category][subcategory]["median"])
+    print(analyzer.analysis["lifetime"][category][subcategory]["median"])
 
     print("\nVariances:")
-    print(analyzer.analysis["2021"]["needs"]["rent"]["variance"])
-    print(analyzer.analysis["2022"]["needs"]["rent"]["variance"])
-    print(analyzer.analysis["2023"]["needs"]["rent"]["variance"])
-    print(analyzer.analysis["lifetime"]["needs"]["rent"]["variance"])
+    print(analyzer.analysis["2021"][category][subcategory]["variance"])
+    print(analyzer.analysis["2022"][category][subcategory]["variance"])
+    print(analyzer.analysis["2023"][category][subcategory]["variance"])
+    print(analyzer.analysis["lifetime"][category][subcategory]["variance"])
 
     print("\nStandard Deviations:")
-    print(analyzer.analysis["2021"]["needs"]["rent"]["standard_deviation"])
-    print(analyzer.analysis["2022"]["needs"]["rent"]["standard_deviation"])
-    print(analyzer.analysis["2023"]["needs"]["rent"]["standard_deviation"])
-    print(analyzer.analysis["lifetime"]["needs"]["rent"]["standard_deviation"])
+    print(analyzer.analysis["2021"][category][subcategory]["standard_deviation"])
+    print(analyzer.analysis["2022"][category][subcategory]["standard_deviation"])
+    print(analyzer.analysis["2023"][category][subcategory]["standard_deviation"])
+    print(analyzer.analysis["lifetime"][category][subcategory]["standard_deviation"])
 
     print("\nRegression Coefficients:")
-    print(analyzer.analysis["2021"]["needs"]["rent"]["linear_regression_coefficients"])
-    print(analyzer.analysis["2022"]["needs"]["rent"]["linear_regression_coefficients"])
-    print(analyzer.analysis["2023"]["needs"]["rent"]["linear_regression_coefficients"])
-    print(analyzer.analysis["lifetime"]["needs"]["rent"]["linear_regression_coefficients"])
+    print(analyzer.analysis["2021"][category][subcategory]["linear_regression_coefficients"])
+    print(analyzer.analysis["2022"][category][subcategory]["linear_regression_coefficients"])
+    print(analyzer.analysis["2023"][category][subcategory]["linear_regression_coefficients"])
+    print(analyzer.analysis["lifetime"][category][subcategory]["linear_regression_coefficients"])
 
     print("\nSome Expected Values:")
-    print(analyzer.get_expected_value("2025", "May", "lifetime", "needs", "rent"))
-    print(analyzer.get_expected_value("2022", "June", "lifetime", "needs", "rent"))
+    print(analyzer.get_expected_value("2025", "May", "lifetime", category, subcategory))
+    print(analyzer.get_expected_value("2022", "June", "lifetime", category, subcategory))
 
 
 if __name__ == "__main__":
